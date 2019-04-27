3a3263599f009e9664b93e68fafdaee113d4da75
chenzeze <847780916@qq.com>
2019-04-27 23:11:06 +0800

    add category page

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,14 +1,15 @@
 // pages/category/category.js
 import { Category } from 'category-model.js';
-var category = new Category();  
+var category = new Category();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    transClassArr: ['tanslate0', 'tanslate1', 'tanslate2', 'tanslate3', 'tanslate4', 'tanslate5'],
+    currentMenuIndex: 0,
     loadingHidden: false,
-
   },
 
   /**
@@ -43,12 +44,69 @@ Page({
     });
   },
 
+  /*切换分类*/
+  changeCategory: function (event) {
+    var index = category.getDataSet(event, 'index'),
+      id = category.getDataSet(event, 'id')//获取data-set
+    this.setData({
+      currentMenuIndex: index
+    });
+
+    //如果数据是第一次请求
+    if (!this.isLoadedData(index)) {
+      var that = this;
+      this.getProductsByCategory(id, (data) => {
+        that.setData(that.getDataObjForBind(index, data));
+      });
+    }
+  },
+
+  isLoadedData: function (index) {
+    if (this.data['categoryInfo' + index]) {
+      return true;
+    }
+    return false;
+  },
+
+  getDataObjForBind: function (index, data) {
+    var obj = {},
+      arr = [0, 1, 2, 3, 4, 5],
+      baseData = this.data.categoryTypeArr[index];
+    for (var item in arr) {
+      if (item == arr[index]) {
+        obj['categoryInfo' + item] = {
+          procucts: data,
+          topImgUrl: baseData.img.url,
+          title: baseData.name
+        };
+
+        return obj;
+      }
+    }
+  },
+
+
+  getProductsByCategory: function (id, callback) {
+    category.getProductsByCategory(id, (data) => {
+      callback && callback(data);
+    });
+  },
+
+  /*跳转到商品详情*/
+  onProductsItemTap: function (event) {
+    var id = category.getDataSet(event, 'id');
+    wx.navigateTo({
+      url: '../product/product?id=' + id
+    })
+  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this._loadData(() => {
+      wx.stopPullDownRefresh()
+    });
   },
 
 
@@ -56,6 +114,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: '我的Shop',
+      path: 'pages/category/category'
+    }
   }
 })
