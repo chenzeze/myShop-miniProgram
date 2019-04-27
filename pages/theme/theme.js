208646f59e7f2163e9bc32bd4f0caf6bdf3f6cd7
chenzeze <847780916@qq.com>
2019-04-27 16:43:26 +0800

    theme page

--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,47 +1,58 @@
 // pages/theme/theme.js
+import { Theme } from 'theme-model.js';
+var theme = new Theme(); //实例化
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    loadingHidden: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.data.titleName = option.name;
+    this.data.id = option.id;
+    wx.setNavigationBarTitle({
+      title: option.name
+    });
+    this._loadData();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.setNavigationBarTitle({
+      title: this.data.titleName
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  /*加载所有数据*/
+  _loadData: function (callback) {
+    var that = this;
+    /*获取单品列表信息*/
+    theme.getProductorData(this.data.id, (data) => {
+      that.setData({
+        themeInfo: data,
+        loadingHidden: true
+      });
+      callback && callback();
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  /*跳转到商品详情*/
+  onProductsItemTap: function (event) {
+    var id = theme.getDataSet(event, 'id');
+    wx.navigateTo({
+      url: '../product/product?id=' + id
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
+  
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
@@ -50,13 +61,7 @@ Page({
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
+  
   /**
    * 用户点击右上角分享
    */
